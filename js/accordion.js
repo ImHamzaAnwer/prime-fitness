638d8483940634d39eddc490dfbdeef4dc3ffd64
imHamzaAnwer <hamza.anwer8@gmail.com>
2017-01-04 13:42:33 +0500

    added all files

---
+++ js/accordion.js
@@ -0,0 +1,16 @@
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].onclick = function(){
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
+//     }
+// }
+
+
+$(document).ready(function(){
+    $('.accordion').click(function(){
+        $('.panel').slideUp();
+    });
+});
